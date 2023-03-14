068af9be6a9f8cd02b0271d6012823ac3d59e2ea
Nono12002 <127423527+Nono12002@users.noreply.github.com>
2023-03-14 17:09:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,30 +25,82 @@ let Bullet_1: game.LedSprite = null
 let Ship: game.LedSprite = null
 Ship = game.createSprite(2, 4)
 let Alien_1 = game.createSprite(0, 0)
-let Set = true
+let Set_1 = 1
+let Alien_2 = game.createSprite(4, 0)
+let Set_2 = 1
+game.setLife(3)
+game.setScore(0)
+basic.forever(function () {
+    while (Set_1 == 1) {
+        for (let index = 0; index < 4; index++) {
+            Alien_1.move(1)
+            basic.pause(1000)
+        }
+        Alien_1.turn(Direction.Right, 90)
+        Alien_1.move(1)
+        basic.pause(1000)
+        Alien_1.turn(Direction.Right, 90)
+        for (let index = 0; index < 4; index++) {
+            Alien_1.move(1)
+            basic.pause(1000)
+        }
+        Alien_1.turn(Direction.Left, 90)
+        Alien_1.move(1)
+        basic.pause(1000)
+        Alien_1.turn(Direction.Left, 90)
+    }
+})
+basic.forever(function () {
+    while (Set_2 == 1) {
+        Alien_2.turn(Direction.Right, 180)
+        for (let index = 0; index < 4; index++) {
+            Alien_2.move(1)
+            basic.pause(1000)
+        }
+        Alien_2.turn(Direction.Left, 90)
+        Alien_2.move(1)
+        basic.pause(1000)
+        Alien_2.turn(Direction.Left, 90)
+        for (let index = 0; index < 4; index++) {
+            Alien_2.move(1)
+            basic.pause(1000)
+        }
+        Alien_2.turn(Direction.Right, 90)
+        Alien_2.move(1)
+        basic.pause(1000)
+        Alien_2.turn(Direction.Right, 90)
+    }
+})
 basic.forever(function () {
     if (Bullet_1.isTouching(Alien_1) || Bullet_2.isTouching(Alien_1)) {
+        game.addScore(1)
         Alien_1.delete()
-        Set = false
+        Set_1 = 0
         Alien_1 = game.createSprite(0, 0)
-        Set = true
+        Set_1 = 1
     }
-    while (Set == true) {
-        for (let index = 0; index < 4; index++) {
-            Alien_1.move(1)
-            basic.pause(1000)
-        }
-        Alien_1.turn(Direction.Right, 90)
-        Alien_1.move(1)
-        basic.pause(1000)
-        Alien_1.turn(Direction.Right, 90)
-        for (let index = 0; index < 4; index++) {
-            Alien_1.move(1)
-            basic.pause(1000)
-        }
-        Alien_1.turn(Direction.Left, 90)
-        Alien_1.move(1)
-        basic.pause(1000)
-        Alien_1.turn(Direction.Left, 90)
+    if (Bullet_1.isTouching(Alien_2) || Bullet_2.isTouching(Alien_2)) {
+        game.addScore(1)
+        Alien_1.delete()
+        Set_1 = 0
+        Alien_1 = game.createSprite(0, 0)
+        Set_1 = 1
+    }
+    if (Alien_1.isTouching(Ship)) {
+        game.removeLife(1)
+        Alien_1.delete()
+        Set_1 = 0
+        Alien_1 = game.createSprite(0, 0)
+        Set_1 = 1
+    }
+    if (Alien_2.isTouching(Ship)) {
+        game.removeLife(1)
+        Alien_2.delete()
+        Set_2 = 0
+        Alien_2 = game.createSprite(4, 0)
+        Set_2 = 1
+    }
+    if (game.isGameOver()) {
+        basic.showNumber(game.score())
     }
 })
